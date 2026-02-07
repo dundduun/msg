b6e5a492d3100fc65fbe4ce9bdb19212172b362b
dundduun <sadfgqr@gmail.com>
2026-02-07 21:15:09 +0300

    feat: load config

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -1,7 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"github.com/dundduun/msg/sso/internal/config"
+)
+
 func main() {
 	// TODO: инициализировать объект конфига
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 
 	// TODO: запустить логгер
 
