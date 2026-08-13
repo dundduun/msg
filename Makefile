67cf361ecb9b0a472cc9b7985715e9fc5c31cd59
dundduun <sadfgqr@gmail.com>
2026-08-13 19:54:22 +0300

    feat: add air (live reload)

--- Makefile
+++
@@ -1,2 +0,0 @@
-runapp:
-	CONFIG_PATH="core/config/local.yaml" DOTENV_PATH=".env" go run core/cmd/main.go
--- core/internal/adapters/http/app/app.go
+++ core/internal/adapters/http/app/app.go
@@ -36,7 +36,7 @@ func (a *App) Start() {
 	r.Use(middleware.Logger)
 	r.Get("/profile/{id}", a.profileHandler.GetProfile)
 
-	a.log.Info("starting server")
+	a.log.Info("starting application")
 	_ = http.ListenAndServe(fmt.Sprintf(":%d", a.port), r)
 	a.log.Info("server stopped")
 }
